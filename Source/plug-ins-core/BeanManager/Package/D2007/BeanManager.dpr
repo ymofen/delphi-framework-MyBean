0246f7abce5d6f040f9034e5b3e96c08ac11a361
ymofen <185511468@qq.com>
2014-05-16 09:56:13 +0800

    hashOf

--- Source/plug-ins-core/BeanManager/Package/D2007/BeanManager.dpr
+++ Source/plug-ins-core/BeanManager/Package/D2007/BeanManager.dpr
@@ -18,7 +18,8 @@ uses
   uLibFactoryObject in '..\..\Service\uLibFactoryObject.pas',
   uApplicationContext in '..\..\Service\uApplicationContext.pas',
   uBaseFactoryObject in '..\..\Service\uBaseFactoryObject.pas',
-  uFactoryInstanceObject in '..\..\Service\uFactoryInstanceObject.pas';
+  uFactoryInstanceObject in '..\..\Service\uFactoryInstanceObject.pas',
+  uKeyMapImpl in '..\..\Service\uKeyMapImpl.pas';
 
 {$R *.res}
 
