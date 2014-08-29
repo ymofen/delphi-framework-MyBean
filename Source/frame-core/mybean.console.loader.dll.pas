b43c65c2cd2f5dc20da29cc05b7b6cc5b87d0fdb
ymofen <185511468@qq.com>
2014-08-29 13:51:43 +0800

    +add console

---
+++ Source/frame-core/mybean.console.loader.dll.pas
@@ -0,0 +1,141 @@
+unit mybean.console.loader.dll;
+
+interface
+
+uses
+  Windows, SysUtils, Classes, mybean.core.intf, superobject, uSOTools,
+  mybean.console.loader;
+
+type
+  /// <summary>
+  ///   DLL文件管理
+  /// </summary>
+  TLibFactoryObject = class(TBaseFactoryObject)
+  private
+    FLibHandle:THandle;
+    FlibFileName: String;
+  private
+
+    procedure doCreatePluginFactory;
+
+    procedure doInitialize;
+    procedure SetlibFileName(const Value: String);
+  public
+    procedure checkInitialize; override;
+
+    procedure cleanup;override;
+
+    /// <summary>
+    ///   根据beanID获取插件
+    /// </summary>
+    function getBean(pvBeanID:string): IInterface; override;
+
+    /// <summary>
+    ///   释放Dll句柄
+    /// </summary>
+    procedure doFreeLibrary;
+
+    /// <summary>
+    ///   加载dll文件
+    /// </summary>
+    function checkLoadLibrary(pvRaiseIfNil: Boolean = true): Boolean;
+
+    /// <summary>
+    ///   DLL文件
+    /// </summary>
+    property libFileName: String read FlibFileName write SetlibFileName;
+  end;
+
+implementation
+
+procedure TLibFactoryObject.doCreatePluginFactory;
+var
+  lvFunc:function:IBeanFactory; stdcall;
+begin
+  @lvFunc := GetProcAddress(FLibHandle, PChar('getBeanFactory'));
+  if (@lvFunc = nil) then
+  begin
+    raise Exception.CreateFmt('非法的Plugin模块文件(%s),找不到入口函数(getBeanFactory)', [self.FlibFileName]);
+  end;
+  FBeanFactory := lvFunc;
+end;
+
+procedure TLibFactoryObject.doFreeLibrary;
+begin
+  FBeanFactory := nil;
+  if FLibHandle <> 0 then FreeLibrary(FLibHandle);
+end;
+
+procedure TLibFactoryObject.doInitialize;
+begin
+  doCreatePluginFactory;
+  FbeanFactory.checkInitalize;
+end;
+
+procedure TLibFactoryObject.checkInitialize;
+var
+  lvConfigStr, lvBeanID:AnsiString;
+  lvBeanConfig:ISuperObject;
+  i: Integer;
+begin
+  if FbeanFactory = nil then
+  begin
+    checkLoadLibrary;
+
+    //将配置传入到beanFactory中
+    for i := 0 to FConfig.A['list'].Length-1 do
+    begin
+      lvBeanConfig := FConfig.A['list'].O[i];
+      lvBeanID := AnsiString(lvBeanConfig.S['id']);
+      lvConfigStr := AnsiString(lvBeanConfig.AsJSon(false, false));
+
+      //配置单个bean
+      FbeanFactory.configBean(PAnsiChar(lvBeanID), PAnsiChar(lvConfigStr));
+    end;
+  end;
+
+  //避免提前释放
+  lvConfigStr := '';
+  lvBeanID:= '';
+end;
+
+function TLibFactoryObject.checkLoadLibrary(pvRaiseIfNil: Boolean = true):
+    Boolean;
+begin
+  if FLibHandle <> 0 then
+  begin
+    Result := true;
+    Exit;
+  end;
+  if not FileExists(FlibFileName) then
+  begin
+    if pvRaiseIfNil then
+    begin
+      raise Exception.Create('文件[' + FlibFileName + ']未找到!');
+    end;
+    Result := false;
+  end else
+  begin
+    FLibHandle := LoadLibrary(PChar(FlibFileName));
+    Result := FLibHandle <> 0;
+    if Result then doInitialize;
+  end;
+end;
+
+procedure TLibFactoryObject.cleanup;
+begin
+  doFreeLibrary;
+end;
+
+function TLibFactoryObject.getBean(pvBeanID:string): IInterface;
+begin
+  result := inherited getBean(pvBeanID);
+end;
+
+procedure TLibFactoryObject.SetlibFileName(const Value: String);
+begin
+  FlibFileName := Value;
+  Fnamespace := FlibFileName;
+end;
+
+end.
--- Source/frame-core/mybean.core.intf.pas
+++ Source/frame-core/mybean.core.intf.pas
@@ -1,8 +1,49 @@
-unit uIBeanFactory;
+unit mybean.core.intf;
 
 interface
 
+
 type
+  /// <summary>
+  ///   接口已经改变需要重新编译所有的DLL和主控台
+  ///     2014年5月15日 20:55:28
+  ///     D10.天地弦
+  ///     添加了 checkFinalize
+  ///     修改了 checkInitialize(pvLoadLib:Boolean);stdcall; 添加了参数
+  /// </summary>
+  IApplicationContext = interface(IInterface)
+    ['{0FE2FD2D-3A21-475B-B51D-154E1728893B}']
+
+    /// <summary>
+    ///   初始化配置(线程不安全),
+    ///      pvLoadLib为true时加载配置文件的同时加载DLL文件(服务端程序推荐)
+    ///               为flase时只加载配置文件(客户端程序推荐)
+    ///      pvUseLibCache为true时copy,dll文件到Plug-ins-cache文件夹然后进行加载
+    ///                   为false时不进行copy，原来目录进行加载
+    /// </summary>
+    procedure checkInitialize; stdcall;
+
+
+    /// <summary>
+    ///   执行反初始化在程序准备退出的时候使用
+    /// </summary>
+    procedure checkFinalize; stdcall;
+
+
+
+    /// <summary>
+    ///   获取一个bean接口(线程安全)
+    ///     如果单实例内部开启互斥
+    /// </summary>
+    function getBean(pvBeanID: PAnsiChar): IInterface; stdcall;
+
+
+    /// <summary>
+    ///   获取beanID对应的工厂接口
+    /// </summary>
+    function getBeanFactory(pvBeanID:PAnsiChar):IInterface; stdcall;
+  end;
+
 
   /// <summary>
   ///   插件工厂接口,由插件宿主(DLL, BPL)库文件提供
@@ -70,6 +111,17 @@ type
     function configBeanSingleton(pvBeanID: PAnsiChar; pvSingleton:Boolean): Integer; stdcall;
   end;
 
+  /// <summary>
+  ///   插件工厂注册, 由beanManager.dll提供
+  /// </summary>
+  IbeanFactoryRegister = interface(IInterface)
+    ['{C06270CF-FF16-4AB0-89D7-3D8C3D8D9820}']
+
+    /// <summary>
+    ///   直接注册Bean工厂插件, 单EXE可以直接注册
+    /// </summary>
+    function registerBeanFactory(const pvFactory: IBeanFactory; const pvNameSapce:PAnsiChar):Integer;stdcall;
+  end;
 
 implementation
 
