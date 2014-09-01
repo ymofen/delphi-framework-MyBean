9c666f1e31972cb4efaaefca1c0bf84224cceca6
ymofen <185511468@qq.com>
2014-09-01 12:45:19 +0800

    * 整理 DIOCP_DBDEMO

---
+++ samples/diocp-DBDEMO/Forms/ufrmMain.pas
@@ -0,0 +1,85 @@
+unit ufrmMain;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics,
+  Controls, Forms, Dialogs, StdCtrls,
+  Grids, DBGrids, uBasePluginForm,
+  mybean.tools.beanFactory,
+  uIRemoteServer, DB, DBClient;
+
+type
+  TfrmMain = class(TBasePluginForm)
+    mmoSQL: TMemo;
+    btnConnect: TButton;
+    edtHost: TEdit;
+    edtPort: TEdit;
+    DBGrid1: TDBGrid;
+    btnOpen: TButton;
+    cdsMain: TClientDataSet;
+    dsMain: TDataSource;
+    procedure btnConnectClick(Sender: TObject);
+    procedure btnOpenClick(Sender: TObject);
+  private
+    { Private declarations }
+    FRemoteSvr:IRemoteServer;
+
+  public
+    { Public declarations }
+
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+
+
+
+
+{$R *.dfm}
+
+{ TfrmMain }
+
+constructor TfrmMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  FRemoteSvr := TMyBeanFactoryTools.getBean('diocpRemoteSvr') as IRemoteServer;
+end;
+
+procedure TfrmMain.btnConnectClick(Sender: TObject);
+begin
+  (FRemoteSvr as IRemoteServerConnector).setHost(PAnsiChar(AnsiString(edtHost.Text)));
+  (FRemoteSvr as IRemoteServerConnector).setPort(StrToInt(edtPort.Text));
+  (FRemoteSvr as IRemoteServerConnector).open;
+  ShowMessage('open succ!');
+end;
+
+procedure TfrmMain.btnOpenClick(Sender: TObject);
+var
+  vData:OleVariant;
+  l : Cardinal;
+begin
+  vData := mmoSQL.Lines.Text;
+
+  l := GetTickCount;
+  if FRemoteSvr.Execute(1, vData) then
+  begin
+    self.cdsMain.Data := vData;
+    Self.Caption := Format('query: count:%d, time:%d',
+      [self.cdsMain.RecordCount, GetTickCount - l]);
+  end;
+end;
+
+destructor TfrmMain.Destroy;
+begin
+  FRemoteSvr := nil;
+  inherited Destroy;
+end;
+
+
+end.
---
+++ samples/diocp-DBDEMO/readme.txt
@@ -0,0 +1,7 @@
+本DEMO服务端依赖于DIOCP3的服务器，可以到DIOCP3的开源上面下载到服务端
+
+开源地址
+https://github.com/ymofen/diocp3
+
+DEMO目录
+samples / socket-Coder / DataModuleDEMO 
